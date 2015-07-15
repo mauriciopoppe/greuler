a3240bb1c1252bc14ef781934a849a46256d09b7
Mauricio Poppe <me@maurizzzio.com>
2015-07-15 02:22:54 -0400

    intro example updated according to the new api

--- app/examples/dfs.js
+++ app/examples/dfs.js
@@ -2,92 +2,57 @@
 
 (function () {
   var greuler = window.greuler;
+
   var instance = greuler({
     target: '#dfs',
     height: 500,
     animationTime: 500,
-    data: {
-      nodes: [
-        {id: 0, x: 0, y: 0},
-        {id: 1, x: 150, y: 150},
-        {id: 2},
-        {id: 3},
-        {id: 4},
-        {id: 5},
-        {id: 6},
-        {id: 7},
-        {id: 8},
-        {id: 9},
-        {id: 10}
-      ],
-      links: [
-        {source: 0, target: 0, weight: 50},
-        {source: 0, target: 0, weight: 70},
-        {source: 0, target: 0, weight: 100},
-        {source: 0, target: 1, weight: 50},
-        {source: 0, target: 1, directed: true},
-        {source: 1, target: 2, weight: 30},
-        {source: 2, target: 3},
-        {source: 3, target: 6},
-        {source: 6, target: 7},
-        {source: 7, target: 10},
-        {source: 0, target: 10},
-        {source: 8, target: 10},
-        {source: 8, target: 9},
-        {source: 9, target: 10},
-        {source: 5, target: 8},
-        {source: 5, target: 7},
-        {source: 7, target: 8},
-        {source: 5, target: 6},
-        {source: 4, target: 5},
-        {source: 3, target: 4},
-        {source: 1, target: 3},
-        {source: 1, target: 6}
-      ]
-    }
+    data: greuler.Graph.random({ connected: true })
   }).update();
 
   window.examples.dfs = function () {
-    var gen = new greuler.player.Generator(instance);
-
-    gen.run(function *algorithm(instance) {
+    var player = new greuler.player.Generator(instance);
+    player.run(function *algorithm(instance) {
       var visited = [];
 
-      function *dfs(u) {
+      function *dfs(u, p) {
         yield function () {
-          instance.selector.highlightNode({ source: u });
+          instance.selector.highlightNode({ id: u });
         };
         visited[u] = true;
 
         var adjacent = instance.graph.getAdjacentNodes(u);
         for (var i = 0; i < adjacent.length; i += 1) {
           var v = adjacent[i].id;
+
+          if (v === p) { continue; }
+
           if (!visited[v]) {
-            yield {
-              type: 'selector',
-              op: 'traverseEdgesBetween',
-              args: [{ source: u, target: v}]
+            yield function () {
+              instance.selector.traverseEdgesBetween({
+                source: u,
+                target: v
+              });
             };
-            yield *dfs(v);
+            yield *dfs(v, u);
           } else {
-            yield {
-              type: 'selector',
-              op: 'traverseEdgesBetween',
-              args: [{ source: u, target: v, keepStroke: false }]
+            yield function () {
+              instance.selector.traverseEdgesBetween({
+                source: u,
+                target: v
+              }, {keepStroke: false}).transition().attr('opacity', 0.4);
             };
           }
         }
 
-        instance.graph.getNode(u).topRight = '✔';
-        instance.update({ skipLayout: true });
-        yield {
-          type: 'selector',
-          op: 'updateNode',
-          args: [{ source: u, fill: 'black' }]
+        yield function () {
+          instance.selector.getNode({ id: u })
+            .transition()
+            .attr('fill', 'black');
         };
       }
 
-      yield *dfs(0);
+      yield *dfs(0, -1);
     });
   };
 })();
